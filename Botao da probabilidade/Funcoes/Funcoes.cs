435f3352dc204f56015119fd2be8931fa93cb23c
Gabriel Ramos <sanchesramosg@gmail.com>
2025-02-13 10:33:35 -0300

    Adicionar arquivos de projeto.

---
+++ Botao da probabilidade/Funcoes/Funcoes.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botao_da_probabilidade.Funcoes;
+
+internal class Funcoes
+{
+    public static int Pontuacao { get; set; }
+    public static int PontuacaoMaxima { get; set; }
+
+
+    public static void ExibirTelaInicial()
+    {
+        Console.Clear();
+        Console.WriteLine(@"       █████                               █████              ███        ███
+      ░░███                               ░░███              ░░░       ███░ 
+       ░███ ██████  ███████ ██████      ███████  ██████              ███░   
+       ░██████░░██████░░██████░░███    ███░░███ ░░░░░███           ███░     
+       ░██░███ ░██░███ ░██░███ ░███   ░███ ░███  ███████         ███░       
+ ███   ░██░███ ░██░███ ░██░███ ░███   ░███ ░███ ███░░███       ███░         
+░░████████░░██████░░██████░░██████    ░░███████░░████████    ███░       ███ 
+ ░░░░░░░░  ░░░░░░  ░░░░░███░░░░░░      ░░░░░░░░ ░░░░░░░░    ░░░        ░░░  
+                   ███ ░███                                                 
+                  ░░██████                                                  
+                   ░░░░░░                                                   ");
+        Console.WriteLine($@"
+                            @@@@@@@@@@@@@@@@@@                            
+                        @@@@                  @@@@                        
+                     @@@                          @@@                    
+                  @@@                                @@@                  
+                 @@                                    @@                
+               @@                                        @@               
+              @@                                          @@              
+             @@                                             @@            
+            @@                                               @@          
+           @@                                                 @@          
+           @@                                                 @@           
+          @@                                                   @@         
+          @@                Pressione 'Espaço'                 @@         
+          @@               para apertar o botão                @@         
+          @@                                                   @@         
+          @@               Pontuação Máxima: {PontuacaoMaxima.ToString().PadLeft(3)}               @@         
+          @@               Pontuação Atual:  {Pontuacao.ToString().PadLeft(3)}               @@         
+          @@                                                   @@         
+           @@                                                 @@          
+           @@                                                 @@          
+            @@                                               @@          
+             @@                                             @@            
+              @@                                           @@            
+               @@                                        @@@              
+                @@@                                     @@                
+                  @@@                                @@@                  
+                    @@@@                           @@@                    
+                       @@@@                    @@@@                       
+                          @@@@@@@@@@@@@@@@@@@@@@                          
+                                   @@@@                                  
+        ");
+        
+    }
+
+    public static void ApertarBotao()
+    {
+        Console.ReadKey();
+
+        int numeroAleatorio = new Random().Next(1, 101);
+
+        if (Pontuacao < numeroAleatorio)
+        {
+            Pontuacao++;
+
+            if (Pontuacao > PontuacaoMaxima)
+            {
+                PontuacaoMaxima = Pontuacao;
+            }
+        }
+        else
+        {
+            Pontuacao = 0;
+        }
+    }
+
+}
---
+++ Botao da probabilidade/Program.cs
@@ -0,0 +1,15 @@
+﻿using Botao_da_probabilidade.Funcoes;
+
+Funcoes.ExibirTelaInicial();
+
+
+while (Funcoes.Pontuacao < 100)
+{
+    Funcoes.ApertarBotao();
+    Funcoes.ExibirTelaInicial();
+}
+
+
+
+
+
